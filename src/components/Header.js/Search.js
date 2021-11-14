6fbc85febfc6f3d43298fd47c2852c706b8d08ed
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-14 15:01:51 +0200

    Building up the project Header component

--- src/components/Header.js/Search.js
+++ src/components/Header.js/Search.js
@@ -4,7 +4,7 @@ const Search = () => {
     return (
     <div className="flex items-center justify-center ">
         <div className="flex border-2 border-gray-200 rounded">
-            <input type="text" className="px-4 text-white py-2 w-80" placeholder="Search..."/>
+            <input type="text" className="px-4 text-black py-2 w-80" placeholder="Search..."/>
             <button className="px-4 text-white bg-gray-600 border-l ">
                 Search
             </button>
