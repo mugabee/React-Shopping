4685dd7cfa0e220f9c163c87e4284ea0787f5e8c
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-15 00:14:15 +0200

    Creating and desgning the Product Filter component

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,11 +1,16 @@
+import { AiFillstar, AiOutlineStar} from 'react-icons/ai'
 const Rating = ({rating, Onclick, style}) => {
     return  <>
     {
         //to successfully let the user choose between 1 to 5  star
         // it is a good idea to start by creating a 5 element array
         [...Array(5)].map((_, i) => (
-            <span>
-            {rating >i}
+            <span key={i} Onclick={() =>onclick(i)} style={style}>
+            {rating > i ? (
+                <AiFillstar/>
+            ) :(
+                <AiOutlineStar/>
+            )}
             </span>
 
         ))
