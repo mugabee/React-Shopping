19f6e6ea3d5efbe1c833798188444be1c49f1139
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-14 15:35:40 +0200

    Building up the project Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header.js/Header'
+import CartDrop from './components/Header.js/CartDrop'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <CartDrop />
       
     </div>
   )
