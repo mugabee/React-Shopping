e4e31c609673bbc7922188bf1cc4990c569e41e8
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-14 23:21:57 +0200

    Creating and desgning the Product Filter component

--- src/components/ProductFilters.js
+++ src/components/ProductFilters.js
@@ -2,35 +2,35 @@ const ProductFilters = () => {
   return (
     <div>
       <span> Filter Products</span>
-      <label className="inline-flex items-center">
+      <label className=" ">
         <input
           type="radio"
-          className="form-radio"
+          
           name="group1"
          
         />
-        <span className="ml-2">Ascending</span>
+        <span className=" ">Ascending</span>
       </label>
       <label className=" ">
         <input
           type="radio"
-          className="form-radio"
-          name="accountType"
+          
+          name="group1"
          
         />
         <span className="ml-2">Descending</span>
       </label>
-      <label className="flex items-center">
-        <input type="checkbox" className="form-checkbox" />
+      <label className=" ">
+        <input type="checkbox"  />
         <span className="ml-2">Include Out of stock</span>
       </label>
       <label className="flex items-center">
-        <input type="checkbox" className="form-checkbox" />
+        <input type="checkbox"  />
         <span className="ml-2">Fast Delivery Only</span>
       </label>
       <button
-        id="navAction"
-        className="mx-auto lg:mx-0 hover:underline bg-blue-200 text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+        
+        className="mx-auto lg:mx-0 hover:underline bg-blue-200 text-white font-bold rounded-3xl mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
       >
         clear
       </button>
