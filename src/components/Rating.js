4429a1af57eb18397d14f6b37ca37c4dcc125e21
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-14 23:56:49 +0200

    Creating and desgning the Product Filter component

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,7 +3,7 @@ const Rating = ({rating, Onclick, style}) => {
     {
         //to successfully let the user choose between 1 to 5  star
         // it is a good idea to start by creating a 5 element array
-        [...Array(5)]
+        [...Array(5)].map
     } </>
 }
 
