0261a6ead9d0e7f7dac099c35e1c1abeedf4a656
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-14 14:55:37 +0200

    Building up the project Header component

--- src/components/Header.js/Search.js
+++ src/components/Header.js/Search.js
@@ -2,14 +2,24 @@ import React from 'react'
 
 const Search = () => {
     return (
-    <div class="flex items-center justify-center ">
-        <div class="flex border-2 border-gray-200 rounded">
+        <div class="container flex mx-auto">
+        <div class="flex border-2 rounded">
+            <button class="flex items-center justify-center px-4 border-r">
+                <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24">
+                    <path
+                        d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z">
+                    </path>
+                </svg>
+            </button>
+            <form>
             <input type="text" class="px-4 py-2 w-80" placeholder="Search...">
-                <button class="px-4 text-white bg-gray-600 border-l ">
-                    Search
-                </button>
-        </div>
+
+
+            </form>
+                   </div>
     </div>
+
     )
 }
 
