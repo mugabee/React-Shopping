28bc6f919755e0363cbe416061365dfbb611c2ab
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-14 15:09:27 +0200

    Building up the project Header component

--- src/components/Header.js/Header.js
+++ src/components/Header.js/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "./Search";
+import CartDrop from "./CartDrop";
 
 const Header = () => {
   return (
@@ -63,7 +64,7 @@ const Header = () => {
             id="navAction"
             class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
           >
-            Action
+            <CartDrop />
           </button>
         </div>
       </div>
