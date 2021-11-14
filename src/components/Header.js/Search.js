897d1effc3b716bb770331235e7b328ce0fc2115
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-14 14:50:59 +0200

    Building up the project Header component

--- src/components/Header.js/Search.js
+++ src/components/Header.js/Search.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Search = () => {
     return (
-        <div class="flex items-center justify-center ">
-    <div class="flex border-2 border-gray-200 rounded">
-        <input type="text" class="px-4 py-2 w-80" placeholder="Search...">
-        <button class="px-4 text-white bg-gray-600 border-l ">
-            Search
-        </button>
+    <div class="flex items-center justify-center ">
+        <div class="flex border-2 border-gray-200 rounded">
+            <input type="text" class="px-4 py-2 w-80" placeholder="Search...">
+                <button class="px-4 text-white bg-gray-600 border-l ">
+                    Search
+                </button>
+        </div>
     </div>
-</div>
     )
 }
 
