5449f3aafcd1018b003a7e696105aeee0c0d5c57
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-14 17:12:32 +0200

    generating project data like products using npm faker

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,10 +1,19 @@
 import { createContext } from "react";
+import faker from "faker";
 
 const ShoppingCart = createContext();
 
 const Context = ({ children }) => {
 
     //here we are using faker.js (a javascript library for generating fake data)
+
+    const products = [...Array(20)].map(() =>({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+
+
+    }));
 
     return <ShoppingCart.Provider>{children}</ShoppingCart.Provider>;
 };
