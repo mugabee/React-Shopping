e93064a6b5191ced44a9a24334750070a9d7ebb4
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-15 14:40:33 +0200

    Add/remove to cart functionality

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,12 @@
 import { CartState } from "../Context/Context";
 
 const CartPage = () => {
-    const
+    const {
+        state: { cart },
+        Dispatch,
+    } =  CartState();
     return (
-        <body className="bg-gray-100">
+<body className="bg-gray-100">
   <div className="container mx-auto mt-10">
     <div className="flex shadow-md my-10">
       <div className="w-3/4 bg-white px-10 py-10">
