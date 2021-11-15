b60f6fbc6483db2efa43856ece8f05f9542a034b
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-16 00:19:00 +0200

    Building up the cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -24,7 +24,7 @@ const CartPage = () => {
   return (
     <div className="container  mx-auto mt-10">
       <div className="flex  shadow-md my-10">
-        <div className="w-3/4 bg-white px-10 py-10">
+        <div className="w-3/4 bg-gray-100 px-10 py-10">
           <div className="flex justify-between border-b pb-8">
             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
             <h2 className="font-semibold text-2xl">{cart.length}</h2>
