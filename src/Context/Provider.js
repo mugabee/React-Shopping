0b983ed8719f66d7351b59ea348f58054aa17c8c
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-15 17:20:18 +0200

    Building up the cart page

--- src/Context/Provider.js
+++ src/Context/Provider.js
@@ -7,7 +7,7 @@ export const cartReducer = (state, action) => {
                 // and manipulate it, I had to 
                 //1. destructuring the current state
                 //2. Add what we are sending to the app        
-            return {...state, cart:[...state.cart, { ...action.payload, quantity:1}]};
+            return {...state, cart:[...state.cart, { ...action.payload, quantity:5}]};
         case "ProductRemoveFromCart": 
         // here I used filter for eact product which was already into the cart
         // and compare each element via Id
