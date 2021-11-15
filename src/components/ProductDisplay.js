f9a406913011eea94a3d1090219772093ceb5dc0
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-15 11:34:19 +0200

    Add/remove to cart functionality

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -23,6 +23,11 @@ const ProductDisplay = ({ product }) => {
       <div className="py-1 text-xl mx-10 grid grid-flow-col auto-cols-max ">
         <Rating rating={product.rating} />
       </div>
+      {
+          //here I used some in order to check if the particular items
+          //exist in array we are using
+          cart.some()
+      }
       <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">remove to cart</button>
       <button disabled={!product.inStock} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">{!product.inStock ? "Out of Stock" : "Add to Cart"}</button>
  
