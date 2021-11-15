71139c1e5699d8190d9d41ccf759e80f7e55c228
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-15 10:05:06 +0200

    Creating and desgning the Product Filter component

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -2,6 +2,7 @@ const ProductDisplay = ({product}) => {
     return <div>
         <img src={product.image} alt={product.name} />
         <h1>{product.name}</h1>
+        <span>{product.price.split(".")[0]} RWF</span>
     </div>;   
 };
 
