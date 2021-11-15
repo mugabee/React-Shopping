2be13664c0195816b97a55a1368af181074a78ba
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-15 13:53:21 +0200

    Add/remove to cart functionality

--- src/components/Header.js/CartDrop.js
+++ src/components/Header.js/CartDrop.js
@@ -4,6 +4,7 @@ import { AiFillDelete } from "react-icons/ai";
 const CartDrop = () => {
   const {
     state: { cart },
+    dispatch,
   } = CartState();
   return (
     <div className="ml-96 my-20">
@@ -71,76 +72,7 @@ const CartDrop = () => {
                     </>
                 ) : (<span> empty</span>)}
 
-             {/* <div className="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100">
-                <div className="p-2 w-12">
-                  <img
-                    src="https://dummyimage.com/50x50/bababa/0011ff&amp;text=50x50"
-                    alt="img product"
-                  />
-                </div>
-                <div className="flex-auto text-sm w-32">
-                  <div className="font-bold">Product 2</div>
-                  <div className="truncate">Product 2 long description</div>
-                  <div className="text-gray-400">Qt: 2</div>
-                </div>
-                <div className="flex flex-col w-18 font-medium items-end">
-                  <div className="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="100%"
-                      height="100%"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="feather feather-trash-2 "
-                    >
-                      <polyline points="3 6 5 6 21 6"></polyline>
-                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                      <line x1="10" y1="11" x2="10" y2="17"></line>
-                      <line x1="14" y1="11" x2="14" y2="17"></line>
-                    </svg>
-                  </div>
-                  $12.22
-                </div>
-              </div> */}
-              {/* <div className="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100">
-                <div className="p-2 w-12">
-                  <img
-                    src="https://dummyimage.com/50x50/bababa/0011ff&amp;text=50x50"
-                    alt="img product"
-                  />
-                </div>
-                <div className="flex-auto text-sm w-32">
-                  <div className="font-bold">Product 3</div>
-                  <div className="truncate">Product 3 description</div>
-                  <div className="text-gray-400">Qt: 2</div>
-                </div>
-                <div className="flex flex-col w-18 font-medium items-end">
-                  <div className="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="100%"
-                      height="100%"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="feather feather-trash-2 "
-                    >
-                      <polyline points="3 6 5 6 21 6"></polyline>
-                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                      <line x1="10" y1="11" x2="10" y2="17"></line>
-                      <line x1="14" y1="11" x2="14" y2="17"></line>
-                    </svg>
-                  </div>
-                  $12.22
-                </div>
-              </div> */}
+             
               <div className="p-4 justify-center flex">
                 <button
                   className="text-base  undefined  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
