0891d643baf4299bf4a1a875b2a91792464a79fa
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-15 10:56:38 +0200

    Creating and desgning the Product Filter component

--- src/Context/Provider.js
+++ src/Context/Provider.js
@@ -1,6 +1,9 @@
-// this switch is going to check the increment action depeding the action
+// this switch is going to check the increment/decrement action depeding the user's action
+
 export const cartReducer = (state, action) => {
     switch (action.type) {
+        case "ProductAddToCart": 
+        return {...state,}
         default:
             return state;
     }
