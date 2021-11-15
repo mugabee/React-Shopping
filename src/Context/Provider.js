460fc9a3adb78967e8ec62218c058bc0b19d6d26
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-15 11:26:19 +0200

    Add/remove to cart functionality

--- src/Context/Provider.js
+++ src/Context/Provider.js
@@ -3,12 +3,18 @@
 export const cartReducer = (state, action) => {
     switch (action.type) {
         case "ProductAddToCart": 
-        //this will return our current state which was already there 
-        // and manipulate it, I had to 
-        //1. destructuring the current state
-        //2. Add what we are sending to the app
-        
-        return {...state, cart:[...state.cart, { ...action.payload, quantity:1}]}
+                //this will return our current state which was already there 
+                // and manipulate it, I had to 
+                //1. destructuring the current state
+                //2. Add what we are sending to the app        
+            return {...state, cart:[...state.cart, { ...action.payload, quantity:1}]};
+        case "ProductRemoveFromCart": 
+        // here I used filter for eact product which was already into the cart
+        // and compare each element via Id
+     
+        return {...state, 
+            cart: state.cart.filter((productInCart) => productInCart.id !== action.payload.id),
+        };
         default:
             return state;
     }
